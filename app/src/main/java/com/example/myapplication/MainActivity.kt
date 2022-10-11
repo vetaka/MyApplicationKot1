c7f1017f881d9dac6a32764ec0b16795aab71d35
Ivan-Kashkin <https://github.com/Ivan-Kashkin/Archi.git>
2022-10-11 16:03:07 +0300

    My first new Project in course Kotlin (less 1) + Kotlin + Convert MainActivity to .kt + Button + CompileSDK=33 11

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.example.myapplication
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+
 import androidx.appcompat.widget.AppCompatButton
-import com.example.myapplication.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<AppCompatButton>(R.id.btn).setOnClickListener { TODO("Not yet implemented") }
+        findViewById<AppCompatButton>(R.id.btn).setOnClickListener { }
 
 
     }
