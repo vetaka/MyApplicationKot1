b8d976f5bae896d9f198cb89adc1dafc20736d96
vetaka <veta-kashkina@yandex.ru>
2022-10-13 16:06:40 +0300

    HomeWork1 с исправлением ошибок

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,45 +1,46 @@
 package com.example.myapplication
 
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.TextView
-
 import androidx.appcompat.widget.AppCompatButton
-import com.google.android.gms.awareness.state.Weather as StateWeather
+import android.util.Log.d
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<AppCompatButton>(R.id.btn).setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                findViewById<AppCompatButton>(R.id.btn).text = "Привет, дорогие "         }
-        })
-
-
-
-        val alex = Person(30, "Alex")
-        val igor = alex.copy(age=35, nameOfPerson ="Igor")
-        findViewById<TextView>(R.id.text_view).text = alex.nameOfPerson + " and " + igor.nameOfPerson
-
-        val weatherList: List<StateWeather>
-
-        for(weather in weatherList) {
-            print(weather.humidity)
+        findViewById<AppCompatButton>(R.id.btn).setOnClickListener {
+            findViewById<AppCompatButton>(R.id.btn).text = "Привет, дорогие "
         }
 
-        for(i in 1..10) {
-                print("Hello Kotlin!")
+
+        val alex = Per(30, "Alex")
+        val igor = alex.copy(age =35, nameOfPerson = "Igor")
+        findViewById<TextView>(R.id.text_view).text =
+            alex.nameOfPerson + " and " + igor.nameOfPerson
+
+
+
+        for (i in 1..10) {
+           d("@@@", "Hello Kotlin!")
         }
 
         for (i in 10 downTo 1 step 2)
             print("Hello Kotlin!")
 
+
+
+
+
+
+
+
     }
-    data class Person(val age: Int, val nameOfPerson: String)
 
 }
+data class Per(val age: Int, val nameOfPerson: String)
